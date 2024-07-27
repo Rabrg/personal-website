10e8debc374f5ea11a2832059204350c41bf5b1f
Ryan Greene <ryangr@openai.com>
2024-07-27 13:09:22 -0700

    blog date format

--- app/components/posts.tsx
+++ app/components/posts.tsx
@@ -18,17 +18,15 @@ export function BlogPosts() {
                 .map((post) => (
                     <Link
                         key={post.slug}
-                        className="flex flex-col space-y-1 mb-4"
+                        className="flex flex-row items-center space-x-4 mb-4"
                         href={`/blog/${post.slug}`}
                     >
-                        <div className="w-full flex flex-col md:flex-row space-x-0 md:space-x-2">
-                            <p className="text-neutral-600 dark:text-neutral-400 w-[100px] tabular-nums">
-                                {formatDate(post.metadata.publishedAt, false)}
-                            </p>
-                            <p className="text-neutral-900 dark:text-neutral-100 tracking-tight">
-                                {post.metadata.title}
-                            </p>
-                        </div>
+                        <p className="text-neutral-600 dark:text-neutral-400 w-24 text-sm tabular-nums whitespace-nowrap">
+                            {formatDate(post.metadata.publishedAt, false)}
+                        </p>
+                        <p className="text-neutral-900 dark:text-neutral-100 tracking-tight flex-grow">
+                            {post.metadata.title}
+                        </p>
                     </Link>
                 ))}
         </div>
