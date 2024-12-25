b785f6895ea08a65a71c65ba0c6b902a1feed838
Ryan Greene <ryangr@openai.com>
2024-12-25 15:14:31 -0600

    quotes

---
+++ app/quotes/page.tsx
@@ -0,0 +1,55 @@
+export const metadata = {
+    title: 'Quotes',
+    description: 'A collection of quotes.'
+  }
+  
+  type Quote = {
+    text: string
+    author: string
+    work?: string
+  }
+  
+  const quotesData: Quote[] = [
+    {
+        text: "When I need, then I shall have, because I know that it is just to give more to whoever asks for more, my demand is my size, my emptiness is my measure.",
+        author: 'Clarice Lispector',
+        work: 'The Passion According to G.H.'
+    },
+    {
+        text: "For love is ever the beginning of Knowledge, as fire is of light.",
+        author: 'Thomas Carlyle',
+        work: 'Death of Goethe'
+    },
+    {
+      text: "The strength of a person's spirit would then be measured by how much 'truth' he could tolerate, or more precisely, to what extent he needs to have it diluted, disguised, sweetened, muted, falsified.",
+      author: 'Friedrich Nietzsche',
+      work: 'Beyond Good and Evil'
+    },
+    {
+        text: "Sapere aude!",
+        author: 'Horace',
+        work: 'First Book of Letters'
+    },
+    {
+        text: "If there be one lesson more than another that should pierce his ear, it is—The world is nothing, the man is all; in yourself is the law of all nature, and you know not yet how a globule of sap ascends; in yourself slumbers the whole of Reason; it is for you to know all; it is for you to dare all.",
+        author: 'Ralph Waldo Emerson',
+        work: 'The American Scholar'
+    },
+  ]
+  
+  export default function QuotesPage() {
+    return (
+      <section>
+        <h1 className="font-semibold text-2xl mb-6 tracking-tighter">Quotes</h1>
+        <ul className="list-disc ml-6">
+          {quotesData.map((quote, index) => (
+            <li key={index} className="mb-2">
+              <span>“{quote.text}”</span> — <span className="font-bold">{quote.author}</span>
+              {quote.work && <span className="italic">, {quote.work}</span>}
+            </li>
+          ))}
+        </ul>
+      </section>
+    )
+  }
+  
